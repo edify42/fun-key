1951229354950c4ed6a4c3149fd48f604e86195d
ted kim <edify42@yahoo.com>
2021-10-02 17:29:48 +1000

    shieeet.
    Signed-off-by: ted kim <edify42@yahoo.com>

--- done.py
+++ done.py
@@ -1,8 +1,5 @@
-# turns out we can leverage the guys library.
-
-import blocksmith
-
+import blocksmithLOCO as blocksmith
 key = '7077da4a47f6c85a21fe6c6cf1285c0fa06915871744ab1e5a5b741027884d00'
 
-address = blocksmith.BitcoinWallet.generate_address(key)
+address = BitcoinWallet.generate_address(key)
 print(address)
